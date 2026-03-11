776ae09f5ea726c9cef6c3676d7382ce181382a6
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-11 16:53:11 +0530

    improvement

--- 08-react-basics-assignment/vite.config.js
+++ 08-react-basics-assignment/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gfg-Assignments/react-basics-assignment/dist/"
+  base: "/gfg-Assignments/08-react-basics-assignment/dist/"
 });
