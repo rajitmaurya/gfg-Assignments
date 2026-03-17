1e21ae4a788215900e5609b3d255c929a908d757
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-17 19:55:32 +0530

    react routing

--- 09-react-routing-assignment/vite.config.js
+++ 09-react-routing-assignment/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === "production"
-    ? "/react-routing-assignment/"
+    ? "/08-react-routing-assignment/"
     : "/"
 })
